991f6ca9caf2d9c04dd55f723afef6d2f5cefa53
WonyoungJang <zwonyoung33@gmail.com>
2024-06-21 05:16:26 +0900

    add commnet

--- 알고리즘/ADT/1_3_Deque/LinkedDeque.c
+++ 알고리즘/ADT/1_3_Deque/LinkedDeque.c
@@ -17,3 +17,28 @@ typedef struct LinkedDeque { // 덱 구조체 선언
     int length; // 덱의 길이
     Dnode* rear; // 마지막 노드를 가리키는 포인터
 } LinkedDeque; // 덱 구조체 명칭 선언
+
+// 연결 덱 초기화
+void LD_init_deque (LinkedDeque* LD) { // 연결 덱의 주소를 매개변수로, 반환값 없음
+    LD->front = LD->rear = NULL; // 첫번째 노드를 가리키는 포인터와 두번째 노드를 가리키는 포인터를 NULL로 초기화
+    LD->length = 0; // 덱의 길이 카운트 초기화
+}
+
+// 연결 덱 항목들을 출력
+void LD_print_deque (LinkedDeque* LD) {
+    printf("front <- "); // 데이터의 출구 표시 출력
+    for(Dnode* nodeCursor = LD->front; nodeCursor; nodeCursor = nodeCursor->next) { // 노드 커서를 만들고 그 노드 커서가 null 즉, 연결 덱의 끝이 아닐 때까지
+        printf("%d <-> ", nodeCursor->data); // 해당 커서 위치의 데이터 출력
+    }
+    printf("rear\n"); // 데이터의 입구 표시 출력 및 개행
+}
+
+// 연결 덱이 포화 상태인지 판단 // 연결 덱은 크기 제한이 없기에 메모리 할당 실패시를 포화 상태로 판단한다
+bool LD_is_deque_full (Dnode *node) { // 노드의 주소를 파라미터로, 참, 거짓 반환
+    return node == NULL; // 노드 할당 성공 여부 참 거짓 결과 반환
+}
+
+// 연결 큐가 공백 상태인지 확인
+bool LD_is_deque_empty (LinkedDeque* LD) { // 연결 큐의 주소를 파라미터로, 참, 거짓 반환
+    return LD->front == NULL; // front가 공백인지에 대한 참 거짓 결과 반환
+}
